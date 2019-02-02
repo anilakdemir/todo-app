a291de767eef869239e2668da6c83ac52771f14e
Anıl Akdemir <anilakdemir1@gmail.com>
2019-02-02 18:02:40 +0000

    Updates

---
+++ precache-manifest.0c9e042e05342659c89312f2f96bba9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "1be1eb4665d89e58447d",
+    "url": "/todo-app/static/js/main.1be1eb46.chunk.js"
+  },
+  {
+    "revision": "1923c0832e77b06fe5ee",
+    "url": "/todo-app/static/js/1.1923c083.chunk.js"
+  },
+  {
+    "revision": "416d23be8f9eeed92cc6c0669fee8891",
+    "url": "/todo-app/index.html"
+  }
+];
